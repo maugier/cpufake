518199a533ed9e316fe06bc48b03c2e302a08d47
Maxime Augier <max@xolus.net>
2011-09-21 19:58:28 +0200

    Should be working now

--- cpufake.c
+++ cpufake.c
@@ -1,14 +1,29 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/smp.h>
 
+#define CM_SIZE 64
 
 static char *cpuname = "B0FHProc 256-bit, LightingTurboBoost enabled";
 
+void set_cpu_name(struct cpuinfo_x86 *c) {
+	strncpy(c->x86_model_id, cpuname, CM_SIZE-1);
+	c->x86_model_id[CM_SIZE-1] = '\0';
+}
+
 int init_module() {
+    loff_t i;
+
     printk(KERN_INFO "cpufake loaded.\n");
+
+    for (i=0; i < nr_cpu_ids; i++) {
+	set_cpu_name(&cpu_data(i));
+    }
+
     return 0;
 }
+
 
 void cleanup_module() {
     printk(KERN_INFO "cpufake unloaded.\n");
@@ -21,3 +36,4 @@ MODULE_LICENSE("GPL");
 
 module_param(cpuname, charp, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
 MODULE_PARM_DESC(cpuname, "Name of CPU to fake");
+
